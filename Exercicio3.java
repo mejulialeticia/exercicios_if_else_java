58447ad9c05ce3cc97566c2344c56b043ec98128
Julia <151403603+mejulialeticia@users.noreply.github.com>
2024-02-25 19:02:36 +0000

    jogo > pedra papel tesoura

---
+++ Exercicio3.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        String[] palavras = {"pedra", "papel", "tesoura"};
+
+        System.out.println(" escolha: >> pedra || papel || tesoura");
+        String jogoUsuario = input.nextLine();
+
+        int indiceAray = random.nextInt(palavras.length);
+        String palavraArray = palavras[indiceAray];
+        System.out.println("");
+        System.out.println(jogoUsuario + " X " + palavraArray);
+        System.out.println("");
+
+        if(jogoUsuario.equals("pedra") && (palavraArray.equals("papel"))){
+            System.out.println("ganhei de você HAHA !!");
+        }else if(jogoUsuario.equals("pedra") && (palavraArray.equals("pedra"))){
+            System.out.println("empatou");
+        }else if(jogoUsuario.equals("pedra") && (palavraArray.equals("tesoura"))){
+            System.out.println("VOCÊ VENCEU");
+        }else if(jogoUsuario.equals("papel") && (palavraArray.equals("papel"))){
+            System.out.println("empatou");
+        }else if(jogoUsuario.equals("papel") && (palavraArray.equals("pedra"))){
+            System.out.println("VOCÊ VENCEU");
+        }else if(jogoUsuario.equals("papel") && (palavraArray.equals("papel"))){
+            System.out.println("empatou");
+        }else if(jogoUsuario.equals("papel") && (palavraArray.equals("tesoura"))){
+            System.out.println("ganhei de você HAHA !!");
+        }else if(jogoUsuario.equals("tesoura") && (palavraArray.equals("pedra"))){
+            System.out.println("ganhei de você HAHA !!");
+        }else if(jogoUsuario.equals("tesoura") && (palavraArray.equals("papel"))){
+            System.out.println("VOCÊ VENCEU");
+        }else if(jogoUsuario.equals("tesoura") && (palavraArray.equals("tesoura"))){
+            System.out.println("empatou");
+        }else{
+            System.out.println("você escreveu errado");
+        }
+        input.close();
+    }
+}
