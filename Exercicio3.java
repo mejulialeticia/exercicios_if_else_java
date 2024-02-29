cb3057746184aeb77c1209d94f41e1913aa59897
Julia <151403603+mejulialeticia@users.noreply.github.com>
2024-02-29 22:04:26 +0000

    pedra papel tesoura

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Exercicio3 {
+public class Exercicio3 {tesoura
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
